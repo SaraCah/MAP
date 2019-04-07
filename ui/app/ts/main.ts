ba061e5333463a91b53c637d12fdb5e2c4c47dd1
Mark Triggs <mark@dishevelled.net>
2019-04-07 16:42:15 +1000

    Vue futzing

--- ui/app/ts/main.ts
+++ ui/app/ts/main.ts
@@ -2,16 +2,33 @@
 
 new Greeter().greet("Hello world");
 
-const Component = Vue.extend({
-    el: '#vuething',
-    template: '<p>{{firstName}} {{lastName}} aka {{alias}}</p>',
-    data: function (): { firstName: string, lastName: string, alias: string } {
+Vue.component('linker', {
+    props: ['name'],
+    template: `
+<div>
+  <p>{{displayString}}</p>
+  <input type="hidden" v-bind:name="name" v-bind:value="selectedUri">{{name}} Here is the linker</p>
+  <input type="text" placeholder="Search" v-on:keyup="handleInput"></input>
+</div>
+`,
+    data: function (): {selectedUri: string, displayString: string} {
         return {
-            firstName: 'MARK',
-            lastName: 'TRIGGS',
-            alias: 'WHATEVER'
-        }
-    }
-})
+            selectedUri: '',
+            displayString: '',
+        };
+    },
+    methods: {
+        handleInput(event: any) {
+            console.log(event.target.value);
 
-new Component();
+            if (event.target.value.length > 10) {
+                this.showPlaceholder("WOO", "RAN IT");
+            }
+        },
+        showPlaceholder(label: string, uri: string) {
+            this.displayString = label;
+            this.selectedUri = uri;
+        },
+    }
+});
+
