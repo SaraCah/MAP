c980b50b7b40d1bf8cef2811fad358f73a9bb4e5
Mark Triggs <mark@dishevelled.net>
2019-04-12 11:16:11 +1000

    Oof our brains

--- api/app/indexer/indexer.rb
+++ api/app/indexer/indexer.rb
@@ -42,13 +42,14 @@ class Indexer
     File.rename(tmp, @state_file)
   end
 
-  def prepare_agent_corporate(row)
+  def prepare_agent_corporate(row, ancestor_ids)
     {
       "id" => "agent_corporate_entity:#{row[:id]}",
       "aspace_id" => row[:id].to_s,
       "display_string" => row[:sort_name],
       "keywords" => row[:sort_name],
-      "record_type" => "agent_corporate_entity"
+      "record_type" => "agent_corporate_entity",
+      "ancestor_ids" => ancestor_ids.map {|id| "agent_corporate_entity:#{id}"},
     }
   end
 
@@ -101,8 +102,8 @@ class Indexer
       AspaceDB.open do |db|
         batch = []
 
-        walk_agency_tree(db, last_mtime) do |agent|
-          batch << prepare_agent_corporate(agent)
+        walk_agency_tree(db, last_mtime) do |agent, ancestor_ids|
+          batch << prepare_agent_corporate(agent, ancestor_ids)
           if batch.length >= BATCH_SIZE
             needs_commit = send_batch(batch) || needs_commit
           end
@@ -129,62 +130,104 @@ class Indexer
                       .filter(Sequel[:agent_corporate_entity][:system_mtime] >= Time.at(last_mtime / 1000))
                       .map{|row| [row[:id]]}
 
-    completed_paths = []
+    return if paths_to_root.empty?
 
-    while(true) do
+    loop do
+      # paths_to_root
+      require 'pp';$stderr.puts("\n*** DEBUG #{(Time.now.to_f * 1000).to_i} [indexer.rb:141 f8f285]: " + {%Q^paths_to_root^ => paths_to_root}.pretty_inspect + "\n")
+
       relns = db[:series_system_rlshp]
+                .join(:enumeration_value, Sequel[:series_system_rlshp][:relator_id] => Sequel[:enumeration_value][:id])
                 .filter(:jsonmodel_type => 'series_system_agent_agent_containment_relationship')
-                .filter(:agent_corporate_entity_id_1 => paths_to_root.map(&:last))
-                .filter(Sequel.~(:end_date => nil))
-                .select(Sequel.as(:agent_corporate_entity_id_0, :parent),
-                        Sequel.as(:agent_corporate_entity_id_1, :child))
+                .filter(Sequel.|({:agent_corporate_entity_id_0 => paths_to_root.map(&:last)},
+                                 {:agent_corporate_entity_id_1 => paths_to_root.map(&:last)}))
+                .filter(:end_date => nil)
+                .select(Sequel.as(:agent_corporate_entity_id_0, :left),
+                        Sequel.as(:agent_corporate_entity_id_1, :right),
+                        Sequel.as(:relationship_target_id, :target),
+                        Sequel.as(Sequel[:enumeration_value][:value], :relator))
 
-      break if relns.empty?
-
+      progressed = false
       relns.each do |row|
+        (parent_id, child_id) = if row[:relator] == 'is_contained_within'
+                                  parent_id = row[:target]
+                                  child_id = ([row[:left], row[:right]] - [parent_id]).first
+                                  [parent_id, child_id]
+                                else
+                                  child_id = row[:target]
+                                  parent_id = ([row[:left], row[:right]] - [child_id]).first
+                                  [parent_id, child_id]
+                                end
+
         paths_to_root.each do |path|
-          if path.last == row[:child]
-            if path.include?(row[:parent])
-              completed_paths << path
+          if path.last == child_id
+            if path.include?(parent_id)
+              # Cycle
             else
-              path << row[:parent]
+              path << parent_id
+              progressed = true
             end
           end
         end
-        paths_to_root -= completed_paths
       end
+
+      break unless progressed
     end
 
-    completed_paths.concat(paths_to_root)
-
     # walk all agency trees from the root agency
-    processed_ids = []
-    left_to_process = completed_paths.map(&:last)
-    while(!left_to_process.empty?) do
+
+    paths_from_root = paths_to_root.map {|path| [path.last]}
+    100.times do
+      to_process = paths_from_root.map(&:last)
+
+      # Yield the agents seen so far
       db[:agent_corporate_entity]
         .join(:name_corporate_entity, Sequel[:agent_corporate_entity][:id] => Sequel[:name_corporate_entity][:agent_corporate_entity_id])
         .filter(Sequel[:name_corporate_entity][:authorized] => 1)
-        .filter(Sequel[:agent_corporate_entity][:id] => left_to_process)
+        .filter(Sequel[:agent_corporate_entity][:id] => to_process)
         .select(Sequel[:agent_corporate_entity][:id],
                 Sequel[:name_corporate_entity][:sort_name])
         .each do |agent|
-
-        yield(agent)
-
-        processed_ids << agent[:id]
+        yield [agent, paths_from_root.find {|path| path.last == agent[:id]}.clone]
       end
 
-      children_to_process = []
-      db[:series_system_rlshp]
-        .filter(:jsonmodel_type => 'series_system_agent_agent_containment_relationship')
-        .filter(:agent_corporate_entity_id_0 => left_to_process)
-        .filter(Sequel.~(:end_date => nil))
-        .select(Sequel.as(:agent_corporate_entity_id_1, :child))
-        .each do |row|
-        children_to_process << row[:child]
+      # Find the next layer of children
+      relns = db[:series_system_rlshp]
+                .join(:enumeration_value, Sequel[:series_system_rlshp][:relator_id] => Sequel[:enumeration_value][:id])
+                .filter(:jsonmodel_type => 'series_system_agent_agent_containment_relationship')
+                .filter(Sequel.|({:agent_corporate_entity_id_0 => to_process},
+                                 {:agent_corporate_entity_id_1 => to_process}))
+                .filter(:end_date => nil)
+                .select(Sequel.as(:agent_corporate_entity_id_0, :left),
+                        Sequel.as(:agent_corporate_entity_id_1, :right),
+                        Sequel.as(:relationship_target_id, :target),
+                        Sequel.as(Sequel[:enumeration_value][:value], :relator))
+
+      progressed = false
+      relns.each do |row|
+        (parent_id, child_id) = if row[:relator] == 'is_contained_within'
+                                  parent_id = row[:target]
+                                  child_id = ([row[:left], row[:right]] - [parent_id]).first
+                                  [parent_id, child_id]
+                                else
+                                  child_id = row[:target]
+                                  parent_id = ([row[:left], row[:right]] - [child_id]).first
+                                  [parent_id, child_id]
+                                end
+
+        paths_from_root.each do |path|
+          if path.last == parent_id
+            if path.include?(child_id)
+              # Cycle.  Ignore.
+            else
+              progressed = true
+              path << child_id
+            end
+          end
+        end
       end
 
-      left_to_process = children_to_process - processed_ids
+      break unless progressed
     end
   end
 
