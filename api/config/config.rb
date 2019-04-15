f12963d938116011de39d2c0e88def0714105d4b
Mark Triggs <mark@dishevelled.net>
2019-04-15 13:49:21 +1000

    Fix config path

--- api/config/config.rb
+++ api/config/config.rb
@@ -2,7 +2,7 @@
 # grant all on map.* to 'map'@'localhost' identified by 'map123';
 
 begin
-  load "../config/config.local.rb"
+  load File.join(File.dirname(__FILE__), "/config.local.rb")
 rescue LoadError
   $stderr.puts("Didn't find config.local.rb")
 end
