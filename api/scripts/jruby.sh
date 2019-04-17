9dfe876154248a77b695b0bb3071205f4878217e
Mark Triggs <mark@dishevelled.net>
2019-04-17 13:55:44 +1000

    Support JAVA_OPTS

--- api/scripts/jruby.sh
+++ api/scripts/jruby.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export JVM_HEAP_SIZE="1024m"
+export JVM_HEAP_SIZE="512m"
 
 cd "`dirname "$0"`/../"
 
 export GEM_HOME=$PWD/distlibs/gems
 
-java -Xmx${JVM_HEAP_SIZE} -cp 'distlibs/*' org.jruby.Main ${1+"$@"}
+java ${JAVA_OPTS} -Xmx${JVM_HEAP_SIZE} -cp 'distlibs/*' org.jruby.Main ${1+"$@"}
