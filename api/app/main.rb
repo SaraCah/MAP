3669aa3b0dea0fa6bcd744b19f0168b337ec59f9
jambun <jambun@gmail.com>
2019-04-18 11:59:49 +1000

    Rough hack to stop indexer and get indexed from aspace - hopefully not too breaky ... sorry

--- api/app/main.rb
+++ api/app/main.rb
@@ -52,7 +52,7 @@ require 'storage/groups'
 require 'storage/agencies'
 require 'storage/locations'
 
-require 'indexer/indexer'
+#require 'indexer/indexer'
 
 require 'endpoints'
 
@@ -67,7 +67,7 @@ class MAPTheAPI < Sinatra::Base
       load File.join(Dir.pwd, 'endpoints.rb')
     end
 
-    WatchDirReloader.new(["indexer"]).start
+#    WatchDirReloader.new(["indexer"]).start
   end
 
   configure do
@@ -98,7 +98,7 @@ class MAPTheAPI < Sinatra::Base
       end
     end
 
-    Indexer.start(AppConfig[:solr_url], MapAPI.base_dir(File.join('data', 'indexer', 'indexer.state')))
+#    Indexer.start(AppConfig[:solr_url], MapAPI.base_dir(File.join('data', 'indexer', 'indexer.state')))
   end
 
   configure :development do
