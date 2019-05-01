b516449708b1d239824fc04922331c14ba511c7a
Mark Triggs <mark@dishevelled.net>
2019-05-01 10:24:55 +1000

    Use /dev/urandom so we don't inadvertently block waiting for the OS entropy pool
    Warning from OpenJDK startup was:
    
         Warning: Available entropy is low. As a result, use of the UUIDField, SSL, or any other features that require
         RNG might not work properly. To check for the amount of available entropy, use 'cat /proc/sys/kernel/random/entropy_avail'.

--- api/scripts/start.sh
+++ api/scripts/start.sh
@@ -72,7 +72,7 @@ trap "stop_solr" INT TERM EXIT
 
 function run() {
     mkdir -p data/solr
-    solr_dist/bin/solr start -p $solr_port -s solr -a "-Dsolr.data.home=$PWD/data/solr"
+    solr_dist/bin/solr start -p $solr_port -s solr -a "-Dsolr.data.home=$PWD/data/solr -Djava.security.egd=file:/dev/./urandom"
     scripts/jruby.sh distlibs/gems/bin/fishwife app/config.ru --host $listen_address --port $listen_port -E "$MAP_ENV"
 }
 
