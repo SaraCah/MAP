b55af6f52f56801645fa42d36605640606da170a
Mark Triggs <mark@dishevelled.net>
2019-05-10 11:41:51 +1000

    Clean up validation error message format

--- api/app/endpoints.rb
+++ api/app/endpoints.rb
@@ -414,7 +414,7 @@ class MAPTheAPI < Sinatra::Base
         csv_validator = MapValidator.new
         csv_validator.run_validations(csv_file, csv_validator.sample_validations)
         csv_validator.notifications.notification_list.each do |notification|
-          errors << "[#{notification.type}](#{notification.source}): #{notification.message}"
+          errors << "#{notification.type} - #{notification.message}"
         end
 
         json_response({'valid' => errors.empty?, 'errors' => errors})
