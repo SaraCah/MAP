bfb6eda7b56fb9f0a80d58fc982b0f6f1c8316fd
Mark Triggs <mark@dishevelled.net>
2019-06-06 10:35:15 +1000

    Remove crap from config

--- api/config/config.rb
+++ api/config/config.rb
@@ -11,9 +11,6 @@ AppConfig[:solr_url] = "http://localhost:8984/solr/map/"
 AppConfig[:solr_indexer_state_file] = File.join(File.dirname(__FILE__), "..", "data/solr_indexer_state.dat")
 AppConfig[:page_size] = 20
 
-#AppConfig[:storage_file_path] = "/tmp/qsa_dev"
-AppConfig[:storage_s3_url] = "https://s3-ap-southeast-2.amazonaws.com/mstqsaqas3bucket"
-
 begin
   load File.join(File.dirname(__FILE__), "/config.local.rb")
 rescue LoadError
