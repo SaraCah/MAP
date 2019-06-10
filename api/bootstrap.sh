d872456f9c03e976138f2aaa5c70e3c054197a0a
Payten Giles <payten.giles@gmail.com>
2019-06-10 11:00:05 +1000

    Updated SOLR download URL

--- api/bootstrap.sh
+++ api/bootstrap.sh
@@ -3,7 +3,7 @@
 JRUBY_VERSION="https://repo1.maven.org/maven2/org/jruby/jruby-complete/9.2.7.0/jruby-complete-9.2.7.0.jar"
 JRUBY_SHA256="a43125f921e707eef861713028d79f60d2f4b024ea6af71a992395ee9e697c22"
 
-SOLR_VERSION="http://apache.mirror.amaze.com.au/lucene/solr/8.0.0/solr-8.0.0.tgz"
+SOLR_VERSION="http://archive.apache.org/dist/lucene/solr/8.0.0/solr-8.0.0.tgz"
 SOLR_SHA256="0e6392d3b980ab917c731b054101aafcebceacc0e5063cb1e305aeeaec911d12"
 
 function fail() {
