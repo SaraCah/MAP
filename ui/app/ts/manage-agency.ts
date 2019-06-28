713b91f576270bb9d94560b058435f1c5e1b03e1
Mark Triggs <mark@dishevelled.net>
2019-06-28 15:39:28 +1000

    Move the heading into its own row

--- ui/app/ts/manage-agency.ts
+++ ui/app/ts/manage-agency.ts
@@ -21,91 +21,98 @@ interface Agency {
 Vue.component('manage-agency', {
     template: `
 <div v-if="initialised">
-  <div class="col s12 m9 l10">
-    <h2>{{this.agency.label}}</h2>
+  <div class="row">
+    <div class="col s12">
+      <h2>{{this.agency.label}}</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col s12 m9 l10">
 
-    <section id="locations" class="scrollspy section">
-      <div class="card">
-        <div class="card-content">
-          <button class="btn right">Add new location</button>
 
-          <h4>Locations</h4>
+      <section id="locations" class="scrollspy section">
+        <div class="card">
+          <div class="card-content">
+            <button class="btn right">Add new location</button>
 
-          <div class="card" v-for="location in this.agency.locations">
-            <div class="card-content">
-              <button class="btn btn-small right">Add user to location</button>
-              <h5>{{location.location.name}}</h5>
-              <table class="highlight" v-if="location.members.length > 0">
-                <thead>
-                  <th>Username</th>
-                  <th>Name</th>
-                  <th>Role</th>
-                  <th>Permissions</th>
-                  <th></th>
-                </thead>
-                <tbody>
-                  <tr v-for="member in location.members">
-                    <td>{{member.username}}</td>
-                    <td>{{member.name}}</td>
-                    <td>{{member.role}}</td>
-                    <td>
-                      <ul>
-                        <li v-for="permission in member.permissions">
-                          {{permission}}
-                        </li>
-                      </ul>
-                    </td>
-                    <td>
-                      <button class="btn btn-small right">Edit</button>
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div v-else>
-                <p>No members for this location</p>
+            <h4>Locations</h4>
+
+            <div class="card" v-for="location in this.agency.locations">
+              <div class="card-content">
+                <button class="btn btn-small right">Add user to location</button>
+                <h5>{{location.location.name}}</h5>
+                <table class="highlight" v-if="location.members.length > 0">
+                  <thead>
+                    <th>Username</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Permissions</th>
+                    <th></th>
+                  </thead>
+                  <tbody>
+                    <tr v-for="member in location.members">
+                      <td>{{member.username}}</td>
+                      <td>{{member.name}}</td>
+                      <td>{{member.role}}</td>
+                      <td>
+                        <ul>
+                          <li v-for="permission in member.permissions">
+                            {{permission}}
+                          </li>
+                        </ul>
+                      </td>
+                      <td>
+                        <button class="btn btn-small right">Edit</button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div v-else>
+                  <p>No members for this location</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <section id="users" class="scrollspy section">
-      <div class="card">
-        <div class="card-content">
-          <h4>Users</h4>
+      <section id="users" class="scrollspy section">
+        <div class="card">
+          <div class="card-content">
+            <h4>Users</h4>
 
-          <table>
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Name</th>
-                <th>Role(s)</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="user in this.mergedUsers">
-                <td>{{user.username}}</td>
-                <td>{{user.name}}</td>
-                <td>
-                  <ul>
-                    <li v-for="role in user.roles">{{role}}</li>
-                  </ul>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+            <table>
+              <thead>
+                <tr>
+                  <th>Username</th>
+                  <th>Name</th>
+                  <th>Role(s)</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="user in this.mergedUsers">
+                  <td>{{user.username}}</td>
+                  <td>{{user.name}}</td>
+                  <td>
+                    <ul>
+                      <li v-for="role in user.roles">{{role}}</li>
+                    </ul>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <pre>{{this.agency}}</pre>
-  </div>
-  <div class="col hide-on-small-only m3 l2">
-    <ul class="section table-of-contents">
-      <li><a href="#locations">Locations</a></li>
-      <li><a href="#users">Users</a></li>
-    </ul>
+      <pre>{{this.agency}}</pre>
+    </div>
+    <div class="col hide-on-small-only m3 l2">
+      <ul class="section table-of-contents">
+        <li><a href="#locations">Locations</a></li>
+        <li><a href="#users">Users</a></li>
+      </ul>
+    </div>
   </div>
 </div>
 <div v-else-if="failed">
