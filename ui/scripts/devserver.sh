4c1aae8e6dec11e43712a15de8c103ff2157e9cf
Mark Triggs <mark@dishevelled.net>
2019-08-06 14:38:54 +1000

    Detect if Ruby is present and avoid tsc_filter if it isn't

--- ui/scripts/devserver.sh
+++ ui/scripts/devserver.sh
@@ -15,7 +15,15 @@ trap "exit" INT TERM
 trap "kill 0" EXIT
 
 rm -f app/buildjs/*
-(tsc --build --watch app/ts/tsconfig.json | scripts/tsc_filter.rb) &
+
+if [ -z "$(command -v ruby)" ]; then
+    # No ruby installed
+    echo "Ruby isn't available.  Any TypeScript errors will appear in this console."
+    tsc --build --watch app/ts/tsconfig.json &
+else
+    # Ruby is available
+    (tsc --build --watch app/ts/tsconfig.json | scripts/tsc_filter.rb) &
+fi
 
 export MAP_ENV=development
 scripts/start.sh
