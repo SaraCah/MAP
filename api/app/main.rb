e3c4e77d7fe39ca88846610b35cfe043114079d5
Mark Triggs <mark@dishevelled.net>
2019-08-23 08:45:57 +1000

    Remove data backfill

--- api/app/main.rb
+++ api/app/main.rb
@@ -119,25 +119,6 @@ class MAPTheAPI < Sinatra::Base
     DB.connect
     AspaceDB.connect
 
-    # TEMPORARY: Add URLs to existing data.  We can remove this eventually.
-    AspaceDB.open do |aspace_db|
-      DB.open do |map_db|
-        map_db[:file_issue_item].each do |fi|
-          updates = {}
-          [:dispatched_by, :received_by].each do |fld|
-            unless fi[fld].nil?
-              agent_id = aspace_db[:user].filter(:name => fi[fld]).get(:agent_record_id) || 1
-              updates[fld] = "/agents/people/#{agent_id}"
-            end
-          end
-
-          unless updates.empty?
-            map_db[:file_issue_item].filter(:id => fi[:id]).update(updates)
-          end
-        end
-      end
-    end
-
     Ctx.open do
       # Bootstrap an admin user if we need one
       unless Users.user_exists?('admin')
